ddcc8e63d5da37ad9217f1fe146df8db01f6c08e
Victoria <victoriaerygina@yandex.ru>
2022-10-22 17:26:46 +0700

    Игра против компьютера

--- Task_1_Game_vs_Bot.py
+++ Task_1_Game_vs_Bot.py
@@ -3,7 +3,6 @@ a) Добавьте игру против бота
 b) * Подумайте как наделить бота ""интеллектом""
 '''
 from random import getrandbits
-from Task_1_Two_people import Current_Situation, Enter_Candies_Quantity
 
 candies = 201
 max_candies = 28
@@ -12,33 +11,35 @@ player_1 = input('Введите ваше имя: ')
 player_2 = 'Компьютер'
 
 print(f'Начинаем игру! Игрок: {player_1}. Ваш соперник: {player_2}.')
-print('Давайте решим - кто же первым начнет игру?')
+print('\nДавайте решим - кто же первым начнет игру?\n')
 
-first_move = bool(getrandbits(1))
-if first_move == 1:
-    print(f'Первый ход делает {player_1}!')
-else:
-    print(f'Первый ход делает {player_2}!')
+first_move = True
+print(f'Первый ход делает {player_2}!')   # Первый ход автоматически делает компьютер, пытаясь реализовать стратегию для победы.
 
-count_player_1 = 0      # Счетчик количества конфет у игрока 1.
-count_player_2 = 0      # Счетчик количества конфет у игрока 2.
+def Enter_Candies_Quantity(name):        # Запрашиваем у игрока ввод количества конфет.
+    k = int(input(f'{name}, какое количество конфет возьмете (от 1 до 28): '))
+    while k < 1 or k > 28:
+        k = int(input(f'Ошибка! {name}, введите количество от 1 до 28: '))
+    return k
+
+def Current_Situation(candies):           # Выводим на экран текущую ситуацию (сколько конфет осталось на столе).
+    print(f'На столе осталось: {candies} конфет(-ы).')
 
 while candies > 28:
-    if first_move:
-        k = Enter_Candies_Quantity(player_1)          # Игрок вводит количество конфет.
-        count_player_1 += k                           # Прибавляем взятые конфеты к счетчику игрока.
+    if first_move == True:
+        k = candies % 29                              # Компьютер забирает остаток от целочисленного деления имеющегося 
+                                                      # количества конфет на количество, которое можно взять за 1 ход + 1.
         candies -= k                                  # Отнимаем от общего количества конфет уже взятые.
-        first_move = False                            # Переход хода к боту.
-        Current_Situation(player_1, k, count_player_1, candies)
+        print(f'\nЯ сделал свой ход и взял {k} конфет(-ы).\n')
+        Current_Situation(candies)
+        first_move = False                            # Переход хода к игроку 1.
     else:
-        k = candies % 29                              # Компьютер забирает остаток от целочисленного деления имеющегося..
-        count_player_2 += k                           # количества конфет на количество, которое можно взять за 1 ход + 1.
+        k = Enter_Candies_Quantity(player_1)          # Игрок вводит количество конфет.
         candies -= k                                  # Отнимаем от общего количества конфет уже взятые.
-        first_move = True                             # Переход хода к игроку 1.
-        print('Я сделал ход, теперь ваша очередь!')
-        Current_Situation(player_2, k, count_player_2, candies)
+        first_move = True                             # Переход хода к боту.
+        Current_Situation(candies)
 
 if first_move:                                        # Определяем победителя, когда осталось <= 28 конфет.
-    print(f'Ура! {player_1}, вы победили!')
+    print(f'Увы, компьютер вас обыграл.')
 else:
-    print(f'Увы, компьютер вас обыграл :)')
+    print(f'Ура! {player_1}, вы победили!')
